3b0f2b536175a9f54b4dd26628f58f78fa417001
kartikworkspace1 <kartik.patel@payarc.com>
2026-03-03 10:38:00 -0500

    Test Commit (#1)
    Test Branch

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -6,8 +6,8 @@ export class LoginPage {
 
   async login() {
     const creds = getEnvCredentials();
-    await this.page.goto('/');
-    await this.page.fill('#email', creds.username);
+    await this.page.goto('https://partnerhub.payarc.io/login');
+    await this.page.fill('#username', creds.username);
     await this.page.fill('#password', creds.password);
     await this.page.click('button[type="submit"]');
   }
