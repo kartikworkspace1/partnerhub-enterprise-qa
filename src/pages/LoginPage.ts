57cd165b74001746830748af65d8b8b2c714295d
kartikworkspace1 <kartik.patel@payarc.com>
2026-03-04 15:27:14 -0500

    Successfully Login Test

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -7,22 +7,21 @@ export class LoginPage {
   async login() {
     const creds = getEnvCredentials();
 
-  
+    // 1. Navigate to the login page
     await this.page.goto('https://partnerhub.payarc.io/login');
     
     const email = this.page.getByTestId("loginUsername");
     const password = this.page.getByTestId("loginPassword");
     const loginButton = this.page.locator('button[type="submit"]');
 
-    console.log("USERNAME:", creds.username);
-    console.log("PASSWORD LENGTH:", creds.password?.length);
-
     // 2. Fill fields and ensure the UI registers the keystrokes
     await email.fill(creds.username);
     await password.fill(creds.password);
 
+   // 3. Wait for the login button to be enabled before clicking
     await expect(loginButton).toBeEnabled({ timeout: 10000 });
 
+    //4. Click the login button
     await loginButton.click();
     
   }
